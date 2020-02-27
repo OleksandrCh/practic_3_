c7101a399eb97092e96f2c5ed31b45bf1056841e
OleksandrCh <oleksandrchernii.0@gmail.com>
2020-02-27 16:12:53 +0200

    sort Name, Zipcode; Delete card

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends React.Component {
     //Получаем данные с jsonplaceholder
     componentDidMount() {
         setTimeout(() => {
-
             fetch('https://jsonplaceholder.typicode.com/users')
                 .then(response => response.json())
                 .then(result => {
@@ -21,30 +20,35 @@ class App extends React.Component {
                     this.setState({data: result})
                 })
         }, 3000)
-};
+    };
 
     //Сортируем по имени
-    sortByAuthorName = () =>{
-        const sort = this.state.data.sort((a, b) =>{ if (a.name > b.name) {
-            return 1;
-        }
-        if (a.name < b.name) {
-            return -1;
-        }
-        return 0;});
+    sortByAuthorName = () => {
+        const sort = this.state.data.sort((a, b) => {
+            if (a.name > b.name) {
+                return 1;
+            }
+            if (a.name < b.name) {
+                return -1;
+            }
+            return 0;
+        });
 
         this.setState({data: sort, sortType: 'byName'})
     };
 
+
     //Сортируем по Зип-коду
-    sortByZipCode = () =>{
-        const sort = this.state.data.sort((a, b) =>{ if (a.address.zipcode > b.address.zipcode) {
-            return 1;
-        }
+    sortByZipCode = () => {
+        const sort = this.state.data.sort((a, b) => {
+            if (a.address.zipcode > b.address.zipcode) {
+                return 1;
+            }
             if (a.address.zipcode < b.address.zipcode) {
                 return -1;
             }
-            return 0;});
+            return 0;
+        });
         this.setState({data: sort, sortType: 'byZip'})
     };
 
@@ -52,23 +56,23 @@ class App extends React.Component {
     removeCard = (e) => {
         console.log(e);
         const filter = this.state.data.filter(user => user.id !== e);
-       this.setState({data: filter})
+        this.setState({data: filter})
     };
+
     //Отрисовываем компоненты
-    render()
-{
-    const {data} = this.state;
-    return (
-        <div className="App">
-            <Header funcSortName={this.sortByAuthorName} funcSortZipCode={this.sortByZipCode}/>
-            {data ? data.map(i => {
-                    return <Card user={i} key={i.id} removeCard={this.removeCard.bind(null,i.id)}/>
-                })
-                : 'Loading...'
-            }
-        </div>
-    );
-}
+    render() {
+        const {data} = this.state;
+        return (
+            <div className="App">
+                <Header funcSortName={this.sortByAuthorName} funcSortZipCode={this.sortByZipCode}/>
+                {data ? data.map(i => {
+                        return <Card user={i} key={i.id} removeCard={this.removeCard.bind(null, i.id)}/>
+                    })
+                    : 'Loading...'
+                }
+            </div>
+        );
+    }
 }
 
 
